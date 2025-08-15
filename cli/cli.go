773ff25ba7ca073ba440116afbf72e02fe15e2fc
Piotr Gulbinowicz <pgulbinowicz@imgw.pl>
2025-08-15 12:43:22 +0200

    add some init cli functions

--- cli/cli.go
+++ cli/cli.go
@@ -1,10 +1,13 @@
 package cli
 
 import (
+	"crypto/tls"
 	"flag"
 	"fmt"
+	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/fatih/color"
 )
@@ -13,6 +16,9 @@ const usage = `Usage:
   plasma create -n <project-name> -c [optional] <compose-file>
 	<compose-file> - default: docker-compose.yml
 `
+
+var baseURL string
+var client *http.Client
 
 func IsCLI() bool {
 	splitted := strings.Split(os.Args[0], "/")
@@ -23,9 +29,24 @@ func IsCLI() bool {
 	return true
 }
 
+func initHttpClient() {
+	client = &http.Client{
+		Timeout: 10 * time.Second,
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+	}
+}
+
+func initBaseURL() {
+	// TODO: read from env or some config file and provide localhost as default
+	baseURL = "http://localhost:8080"
+}
+
 func Run() {
 	createCmd := flag.NewFlagSet("create", flag.ExitOnError)
-
+	initHttpClient()
+	initBaseURL()
 	if len(os.Args) < 2 {
 		fmt.Print(usage)
 		os.Exit(1)
