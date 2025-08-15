11355d8f500d5c1431d23ad3e40bccb78b73957a
Piotr Gulbinowicz <pgulbinowicz@imgw.pl>
2025-08-15 11:25:47 +0200

    add check for executable name

--- main.go
+++ main.go
@@ -4,21 +4,26 @@ import (
 	"log"
 	"time"
 
+	"github.com/pgulb/plasma/cli"
 	"github.com/pgulb/plasma/controller"
 	"github.com/pgulb/plasma/db"
 	"github.com/pgulb/plasma/server"
 )
 
 func main() {
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	go server.Run()
-	for {
-		if db.DB != nil {
-			break
+	if cli.IsCLI() {
+		cli.Run()
+	} else {
+		log.SetFlags(log.LstdFlags | log.Lshortfile)
+		go server.Run()
+		for {
+			if db.DB != nil {
+				break
+			}
+			time.Sleep(1 * time.Second)
 		}
-		time.Sleep(1 * time.Second)
+		go controller.Run()
+		c := make(chan int)
+		<-c
 	}
-	go controller.Run()
-	c := make(chan int)
-	<-c
 }
