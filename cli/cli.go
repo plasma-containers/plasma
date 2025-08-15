fb02c2a196afb925c33fb30ae4ddf9b0efe5e01d
Piotr Gulbinowicz <pgulbinowicz@imgw.pl>
2025-08-15 13:40:39 +0200

    add sending request from create cmd

--- cli/cli.go
+++ cli/cli.go
@@ -2,14 +2,17 @@ package cli
 
 import (
 	"crypto/tls"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"strings"
 	"time"
 
 	"github.com/fatih/color"
+	"github.com/pgulb/plasma/server"
 )
 
 const usage = `Usage:
@@ -19,6 +22,11 @@ const usage = `Usage:
 
 var baseURL string
 var client *http.Client
+
+type QueryParams struct {
+	Compose *string `json:"compose"`
+	Project *string `json:"project"`
+}
 
 func IsCLI() bool {
 	splitted := strings.Split(os.Args[0], "/")
@@ -40,7 +48,38 @@ func initHttpClient() {
 
 func initBaseURL() {
 	// TODO: read from env or some config file and provide localhost as default
-	baseURL = "http://localhost:8080"
+	// for now it's hard-coded in-docker addr
+	baseURL = "http://plasma-server:8080"
+}
+
+func reqDo(method string, url string, qp *QueryParams) (*server.RespMsg, int, error) {
+	req, err := http.NewRequest(method, baseURL+url, nil)
+	if err != nil {
+		return &server.RespMsg{}, 0, err
+	}
+	q := req.URL.Query()
+	if qp.Compose != nil {
+		q.Add("compose", *qp.Compose)
+	}
+	if qp.Project != nil {
+		q.Add("project", *qp.Project)
+	}
+	req.URL.RawQuery = q.Encode()
+	resp, err := client.Do(req)
+	if err != nil {
+		return &server.RespMsg{}, 0, err
+	}
+	defer resp.Body.Close()
+	var msg server.RespMsg
+	content, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return &server.RespMsg{}, 0, err
+	}
+	err = json.Unmarshal(content, &msg)
+	if err != nil {
+		return &server.RespMsg{}, 0, err
+	}
+	return &msg, resp.StatusCode, nil
 }
 
 func Run() {
@@ -54,7 +93,28 @@ func Run() {
 
 	switch os.Args[1] {
 	case "create":
+		projName := flag.String("n", "", "project name to create")
 		createCmd.Parse(os.Args[2:])
-		color.Magenta("Creating project...")
+		if projName == nil {
+			fmt.Print(usage)
+			os.Exit(1)
+		}
+		if *projName == "" {
+			fmt.Print(usage)
+			os.Exit(1)
+		}
+		color.Magenta("Creating project...\n")
+		msg, status, err := reqDo("POST", "/create", &QueryParams{})
+		if err != nil {
+			color.Magenta(msg.Msg)
+			color.Red(err.Error())
+			os.Exit(1)
+		}
+		if status != 201 {
+			color.Red(msg.Msg)
+			color.Red(fmt.Sprintf("HTTP status code %v", status))
+			os.Exit(1)
+		}
+		color.Magenta(msg.Msg)
 	}
 }
