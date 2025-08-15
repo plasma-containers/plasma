27b205b4a65412dfc7e6b588ec2be2a4467517cc
Piotr Gulbinowicz <pgulbinowicz@imgw.pl>
2025-08-15 17:41:57 +0200

    handle /ps

--- server/server.go
+++ server/server.go
@@ -15,6 +15,18 @@ type RespMsg struct {
 	Msg string `json:"msg"`
 }
 
+type CtrStatus struct {
+	Name   string `json:"name"`
+	Status string `json:"status"`
+}
+
+type PsResp struct {
+	Projects []db.Project `json:"projects"`
+	Services []db.Service `json:"services"`
+	Volumes  []db.Volume  `json:"volumes"`
+	Statuses []CtrStatus  `json:"statuses"`
+}
+
 func Msg(msg string) []byte {
 	b, _ := json.Marshal(RespMsg{Msg: msg})
 	return b
@@ -64,6 +76,38 @@ func Create(w http.ResponseWriter, r *http.Request) {
 	w.Write(Msg(fmt.Sprintf("Project '%s' created", projName)))
 }
 
+func Ps(w http.ResponseWriter, r *http.Request) {
+	projs, svcs, vols, err := db.Ps()
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(Msg(err.Error()))
+		return
+	}
+	statuses := []CtrStatus{}
+	for _, svc := range svcs {
+		// TODO: change to ContainerList, will be probably faster and less prone to
+		// listing containers that were just killed by controller
+		ctr, err := container.Get(svc.Name)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(Msg(err.Error()))
+			return
+		}
+		statuses = append(statuses, CtrStatus{Name: svc.Name, Status: ctr.State.Status})
+	}
+	psResp := PsResp{Projects: projs, Services: svcs, Volumes: vols, Statuses: statuses}
+	b, err := json.Marshal(psResp)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(Msg(err.Error()))
+		return
+	}
+	w.Write(Msg(string(b)))
+}
+
 func Run() {
 	log.Println("oOoOo Starting plasma-server oOoOo")
 
@@ -76,6 +120,7 @@ func Run() {
 
 	mux.Handle("GET /healthz", LoggerMiddleware(http.HandlerFunc(Health)))
 	mux.Handle("POST /create", LoggerMiddleware(http.HandlerFunc(Create)))
+	mux.Handle("GET /ps", LoggerMiddleware(http.HandlerFunc(Ps)))
 
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
